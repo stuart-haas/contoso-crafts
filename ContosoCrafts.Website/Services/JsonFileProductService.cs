aa9623c093547be12ad60d21463023ccba576767
Stuart <stuart.haas2@gmail.com>
2021-07-22 07:00:46 -0400

    Initial commit

---
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.Website.Models;
+using System.Text.Json;
+
+namespace ContosoCrafts.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
